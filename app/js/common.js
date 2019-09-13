cc2d3a24bf8ffbd99f5fac81f7829aecdf1132d3
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-09-14 00:12:47 +0300

    add text html

--- app/js/common.js
+++ app/js/common.js
@@ -12,4 +12,37 @@ $(document).ready(function() {
 		arrows: true,
 		dots: false
 	});
+
+	//menu
+	$( ".menu-trigger button" ).click(function() {
+		$( ".menu" ).addClass( "active" );
+		$( ".menu__overlay" ).addClass( "active" );
+	});
+	$( ".menu__close button, .menu__overlay" ).click(function() {
+		$( ".menu" ).removeClass( "active" );
+		$( ".menu__overlay" ).removeClass( "active" );
+	});
+
+	//accordion
+	$( ".menu-slider__trigger" ).click(function() {
+		$( this ).next().slideToggle( "fast" );
+	});
+
+	//scroll anchor
+    $('.scrollto').on('click',function(){
+		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top-5},800);
+		return false;
+	});
+
+
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 700) {
+        $(".scroll-arrow").addClass("active");
+    } else {
+        $(".scroll-arrow").removeClass("active");
+    }
 });
